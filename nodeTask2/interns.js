5bd0885e5d77037c2bad22911043f38137fa6571
kamzzy <preciousakams@yahoo.com>
2020-04-15 19:35:53 +0100

    update

--- nodeTask2/interns.js
+++
@@ -1,8 +0,0 @@
-var mongo = require('mongodb');
-var MongoClient = require('mongodb').MongoClient;
-var url = "mongodb://localhost:27017/ziri";
-MongoClient.connect(url, function(err, db) {
-  if (err) throw err;
-  console.log("Database of interns");
-  db.close();
-});
